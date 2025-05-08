863fc09aa7043a2198de31f688ef19c9ce91de08
JCayllahuaG <u202019558@upc.edu.pe>
2025-05-08 00:54:06 -0500

    Initialize Spring Boot project with Gradle setup and basic application structure

---
+++ src/main/java/com/test/test_script_dependencies/TestScriptDependenciesApplication.java
@@ -0,0 +1,13 @@
+package com.test.test_script_dependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestScriptDependenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestScriptDependenciesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/test_script_dependencies/TestScriptDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.test_script_dependencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestScriptDependenciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
